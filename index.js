e63ec10cd9956b320b7b9fe9cbc47c3304708049
prajwol <prajwolr88@gmail.com>
2024-06-14 21:47:14 +0545

    color changed

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ gsap.to(".nav", {
    
     backgroundColor: "rgba(255, 255, 255, 0.2)",
     boxShadow:"0 4px 30px rgba(0, 0, 0, 0.1)",
-    backdropFilter: "blur(10px)",
+    backdropFilter: "blur(60px)",
     webkitBackdropFilter: "blur(5px)",
     height: "100px",
 
