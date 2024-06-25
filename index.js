0d60198e54656ee5029fd712cb767390f6ec7a72
prajwol <prajwolr88@gmail.com>
2024-06-25 12:38:16 +0545

    reviews section added

--- index.js
+++ index.js
@@ -134,3 +134,27 @@ opacity:1
 
     
 })
+
+
+
+
+//reviews:
+
+// initialize swiper js
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+     // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+})
